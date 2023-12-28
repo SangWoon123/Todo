f7ae61f99112304ec92c8b1ca8786dabeb22806e
SangWoon123 <alrax3574@naver.com>
2023-12-29 00:37:36 +0900

    초기세팅

---
+++ todo-back/src/main/groovy/com/gdsc/todo/TodoApplication.groovy
@@ -0,0 +1,13 @@
+package com.gdsc.todo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TodoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TodoApplication, args)
+	}
+
+}
