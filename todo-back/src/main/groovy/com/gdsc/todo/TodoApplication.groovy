d3fa9a8ff0f2c1b3185267e76e6dca687ecb1846
SangWoon123 <alrax3574@naver.com>
2024-01-10 00:05:14 +0900

    [back] feat: TodoHistory 기능 추가2

--- todo-back/src/main/groovy/com/gdsc/todo/TodoApplication.groovy
+++ todo-back/src/main/groovy/com/gdsc/todo/TodoApplication.groovy
@@ -2,8 +2,11 @@ package com.gdsc.todo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
+@EnableScheduling
 class TodoApplication {
 
 	static void main(String[] args) {
--- todo-back/src/main/groovy/com/gdsc/todo/controller/HistoryController.java
+++ todo-back/src/main/groovy/com/gdsc/todo/controller/HistoryController.java
@@ -1,2 +1,26 @@
-package com.gdsc.todo.controller;public class HistoryController {
+package com.gdsc.todo.controller;
+
+import com.gdsc.todo.dto.response.TodoHistoryResponse;
+import com.gdsc.todo.service.TodoHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/history")
+public class HistoryController {
+
+    private final TodoHistoryService historyService;
+
+    @GetMapping()
+    public ResponseEntity<List<TodoHistoryResponse>> getRecentHistory(){
+        List<TodoHistoryResponse> recentHistory = historyService.getRecentHistory();
+        return new ResponseEntity<>(recentHistory,HttpStatus.OK);
+    }
 }
