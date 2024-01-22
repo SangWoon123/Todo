6f6a2e4128cd6200ccdc6773ef397393eb9cde81
SangWoon123 <alrax3574@naver.com>
2024-01-22 19:45:19 +0900

    [back] feat: security

---
+++ todo-back/src/main/groovy/com/gdsc/todo/global/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.gdsc.todo.global.config;
+
+import com.gdsc.todo.global.oauth.CustomOAuth2Service;
+import com.gdsc.todo.global.oauth.Oauth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomOAuth2Service customOAuth2Service;
+    private final Oauth2SuccessHandler oauth2SuccessHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests)->authorizeRequests
+                        .requestMatchers(
+                                "/"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(oauth2Login-> oauth2Login
+                        .userInfoEndpoint(userInfoEndpointConfig-> userInfoEndpointConfig
+                                        .userService(customOAuth2Service))
+                                .successHandler(oauth2SuccessHandler))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
