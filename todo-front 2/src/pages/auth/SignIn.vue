360ed638115e483f48fc8136b81c77a8dc62224a
SangWoon123 <alrax3574@naver.com>
2024-02-25 15:25:54 +0900

    fix: 프론트 로그인버전 수정

--- todo-front 2/src/pages/auth/SignIn.vue
+++ todo-front 2/src/pages/auth/SignIn.vue
@@ -1,92 +1,149 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="column" style="height: 100vh">
+  <q-page>
+    <div class="column justify-center" style="height: 100vh">
       <!-- 로고 -->
-      <div
-        class="col-4 text-h2 flex flex-center q-mb-xl"
-        style="font-family: 'Itim', cursive"
-      >
-        <span>My</span>
-        <span class="main-color">Todo</span>
-      </div>
-
-      <div class="col">
-        <q-form>
-          <div class="q-gutter-y-sm">
-            <q-input
-              color="orange"
-              label-color="orange"
-              outlined
-              v-model="ph"
-              label="ID"
-              :dense="dense"
-              class="main-color input-size"
-            />
-            <q-input
-              color="orange"
-              label-color="orange"
-              outlined
-              v-model="ph"
-              label="비밀번호"
-              :dense="dense"
-              class="input-size"
+      <span class="logo" style="margin-bottom: 100px">
+        <div class="text-center">
+          <div>
+            <img
+              src="../../assets/MyTodo.png"
+              style="width: 41px; height: 41px"
             />
           </div>
-
-          <div class="column q-mt-sm flex flex-center">
-            <!-- 회원가입 안내 메시지 -->
-            <q-btn class="text-black q-btn2" flat size="10px" to="/auth/sign-up"
-              ><span class="text-primary q-btn2"
-                >회원가입이 필요하신가요?</span
-              ></q-btn
-            >
-            <!-- 비밀번호 잊어버림 안내 메시지 -->
-            <q-btn class="text-black" flat size="10px" to="/"
-              ><span class="text-primary q-btn2"
-                >비밀번호를 잊어버리셨나요?</span
-              ></q-btn
-            >
-            <!-- 로그인 버튼 -->
-            <router-link to="home">
-              <q-btn
-                class="q-px-xl text-black bg-color login"
-                style="color: white; width: 230px; height: 30px"
-                unelevated
-                size="10px"
-                ><span class="text-white">로그인</span></q-btn
-              >
-            </router-link>
-          </div>
-        </q-form>
-        <!-- 구분선 -->
-        <q-separator class="q-my-md" inset style="size: 16px" />
-
-        <!-- 카카오, 네이버 Oauth-->
+        </div>
+        <div class="text-center text-h2">
+          <span>My</span>
+          <span class="main-color">Todo</span>
+        </div>
+      </span>
+      <!-- 버튼 -->
+      <div>
+        <!-- 카카오 Oauth-->
         <div class="column q-gutter-sm flex flex-center">
           <q-btn
             class="q-px-xl text-black bg-color login"
             color="yellow"
-            style="color: white; width: 230px; height: 30px"
+            style="
+              color: white;
+              width: 230px;
+              height: 40px;
+              border-radius: 12px;
+            "
             unelevated
             size="10px"
+            href="http://localhost:8080/oauth2/authorization/kakao"
             ><div class="kakao">
               <img class="absolute-left" src="src/assets/kakao.png" />
               <span> Kakao로 시작하기 </span>
-            </div></q-btn
-          >
+            </div>
+          </q-btn>
 
+          <!-- 구분선 -->
+          <div
+            class="q-py-sm q-pb-lg"
+            style="width: 230px; height: 12px; position: relative"
+          >
+            <div
+              style="
+                width: 230px;
+                height: 0px;
+                left: 0px;
+                top: 8px;
+                position: absolute;
+                border: 0.5px #dadada solid;
+              "
+            ></div>
+            <div
+              style="
+                width: 10px;
+                height: 9px;
+                left: 110px;
+                top: 3px;
+                position: absolute;
+                background: white;
+              "
+            ></div>
+            <div
+              style="
+                left: 110px;
+                top: 0px;
+                position: absolute;
+                color: #a9a9a9;
+                font-size: 10px;
+                font-family: Inter;
+                font-weight: 400;
+                word-wrap: break-word;
+              "
+            >
+              or
+            </div>
+          </div>
+          <!-- 네이버 Oauth -->
           <q-btn
             class="q-px-xl text-black bg-color login"
-            style="color: white; width: 230px; height: 30px"
+            style="
+              color: white;
+              width: 230px;
+              height: 40px;
+              border-radius: 12px;
+            "
             unelevated
             color="positive"
             size="10px"
           >
             <div class="naver">
-              <img class="q-ma-xs absolute-left" src="src/assets/naver.png" />
+              <img class="q-ma-sm absolute-left" src="src/assets/naver.png" />
               <span class="q-pl-sm"> Naver로 시작하기 </span>
             </div>
           </q-btn>
+          <!-- 구글 Oauth -->
+          <q-btn
+            class="q-px-xl text-black login login-btn-google-github"
+            unelevated
+            color="white"
+            size="10px"
+          >
+            <div class="google">
+              <img class="q-ma-sm absolute-left" src="src/assets/google.png" />
+              <span class="q-pl-sm"> Google로 시작하기 </span>
+            </div>
+          </q-btn>
+          <!-- 깃헙 Oauth -->
+          <q-btn
+            class="q-px-xl text-black login login-btn-google-github"
+            unelevated
+            color="white"
+            size="10px"
+          >
+            <div class="github">
+              <img class="q-ma-sm absolute-left" src="src/assets/github.png" />
+              <span class="q-pl-sm"> GitHub으로 시작하기 </span>
+            </div>
+          </q-btn>
+        </div>
+      </div>
+      <!-- 이용약관 -->
+      <div class="text-center q-mt-lg">
+        <div style="opacity: 0.3">
+          <span
+            style="
+              color: black;
+              font-size: 12px;
+              font-family: Inter;
+              font-weight: 400;
+              word-wrap: break-word;
+            "
+            >이용약관 </span
+          ><span
+            style="
+              color: black;
+              font-size: 12px;
+              font-family: Inter;
+              font-weight: 700;
+              word-wrap: break-word;
+            "
+            >개인정보처리방침</span
+          >
         </div>
       </div>
     </div>
@@ -95,12 +152,17 @@
 
 <script>
 import { ref } from "vue";
+import axios from "axios";
 
 export default {
   setup() {
     return {
       dense: ref(true),
     };
+  },
+  methods: {
+    loginWithKakao() {
+    },
   },
 };
 </script>
@@ -116,31 +178,34 @@ export default {
 </style>
 
 <style lang="scss" scoped>
+.logo {
+  font-family: "Itim", cursive;
+}
 .main-color {
   color: $main;
 }
 
-.bg-color {
-  background-color: $main;
+.login-btn-google-github {
+  border-radius: 12px;
+  color: white;
+  width: 230px;
+  height: 40px;
+  border: 1px solid black;
 }
 
-.input-size {
-  width: 250px;
-}
-
-.q-btn2 {
-  border: none;
-  text-decoration: underline;
-  text-decoration-color: $primary;
-  cursor: pointer;
-}
-
+.github,
+.google,
 .naver,
 .kakao {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
+}
+
+.github > img,
+.google > img {
+  width: 25px;
 }
 .naver > img {
   width: 25px;
