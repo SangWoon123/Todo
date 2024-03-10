5728b2eda289da20b5fc9f05f8c0a3cb86e2cd38
SangWoon123 <alrax3574@naver.com>
2024-03-10 15:14:42 +0900

    feat: 토큰만료시간

--- todo-front 2/src/pages/auth/RedirectPage.vue
+++ todo-front 2/src/pages/auth/RedirectPage.vue
@@ -1,21 +1,31 @@
 <template>
-    <div></div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        accessToken: this.$route.query.accessToken,  // 라우트 매개변수에 접근
-        refreshToken: this.$route.query.refreshToken,  // 라우트 매개변수에 접근
-      };
-    },
-    mounted() {
-      localStorage.clear();
-      localStorage.setItem("accessToken", this.accessToken);
-      localStorage.setItem("refreshToken", this.refreshToken);
-      this.$router.replace("/home");  // 홈으로 리다이렉트
-    },
-  };
-  </script>
-  
+  <div></div>
+</template>
+
+<script>
+import { useTokenStore } from "src/stores/token";
+
+export default {
+  data() {
+    return {
+      accessToken: this.$route.query.accessToken, // 라우트 매개변수에 접근
+      refreshToken: this.$route.query.refreshToken, // 라우트 매개변수에 접근
+    };
+  },
+  mounted() {
+    localStorage.clear();
+    localStorage.setItem("accessToken", this.accessToken);
+    localStorage.setItem("refreshToken", this.refreshToken);
+    // 상태업데이트
+    this.token.accessToken=this.accessToken;
+    this.token.refreshToken=this.refreshToken;
+
+    this.$router.replace("/home"); // 홈으로 리다이렉트
+  },
+  setup() {
+    const token = useTokenStore();
+
+    return { token };
+  },
+};
+</script>
