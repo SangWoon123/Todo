6506949779a722f15331e8f7ee2873075d6f240a
SangWoon123 <alrax3574@naver.com>
2024-03-10 15:15:07 +0900

    feat: 로그아웃버튼 추가

--- todo-front 2/src/pages/TodoApp.vue
+++ todo-front 2/src/pages/TodoApp.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="q-ma-xl column">
     <!-- 이동버튼 -->
-    <nav class="q-mb-md">
+    <nav class="flex justify-between q-mb-md">
       <router-link to="sign">
         <q-btn
           color="grey-6"
@@ -11,9 +11,18 @@
           icon="arrow_back_ios"
         ></q-btn>
       </router-link>
+
+      <q-btn
+        icon="logout"
+        unelevated
+        flat
+        color="grey-6"
+        size="10px"
+        @click="confirm"
+      ></q-btn>
     </nav>
 
-     <!-- 헤더 -->
+    <!-- 헤더 -->
     <TodoHeader @add-task="addTask" />
     <!-- 메인컨텐츠 -->
     <TaskList :tasks="tasks" @delete-task="deleteTask" />
@@ -23,15 +32,15 @@
 </template>
 
 <script>
+import { useQuasar } from "quasar";
 import TaskList from "./content/TaskList.vue";
 import TodoHeader from "./content/TodoHeader.vue";
 import TodoHistory from "./content/history/TodoHistory.vue";
+import { useTokenStore } from "src/stores/token";
 
 export default {
   data() {
-    return {
-     
-    };
+    return {};
   },
   methods: {
     // 추가
@@ -54,6 +63,27 @@ export default {
     },
   },
   components: { TodoHeader, TodoHistory, TaskList },
+  setup() {
+    const $q = useQuasar();
+    const token = useTokenStore();
+
+    function confirm() {
+      $q.dialog({
+        message: "정말로 로그아웃 하시겠습니까?",
+        cancel: true,
+        persistent: true,
+      }).onOk(() => {
+        token.accessToken = null;
+        token.refreshToken = null;
+        localStorage.clear();
+        console.log("로그인 완료");
+        console.log(token.accessToken);
+        console.log(token.refreshToken);
+        window.location.href = "/sign";
+      });
+    }
+    return { confirm, token };
+  },
 };
 </script>
 
--- todo-front 2/src/pages/content/day/TodayDate.vue
+++ todo-front 2/src/pages/content/day/TodayDate.vue
@@ -9,6 +9,8 @@
 
 <script>
 import axios from "axios";
+import { useTokenStore } from "src/stores/token";
+
 export default {
   data() {
     return {
@@ -30,20 +32,47 @@ export default {
   },
   created() {
     const url = "http://localhost:8080/member";
-    const accessToken=localStorage.getItem('accessToken');
-    axios
-      .get(url,{
-        headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-      })
-      .then((response) => {
-        const userName = response.data.loginId;
-        this.userName=userName;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    var accessToken = localStorage.getItem("accessToken");
+    var refreshToken = localStorage.getItem("refreshToken");
+    if (this.token.isTokenExpired(accessToken)) {
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            refreshToken: `Bearer ${refreshToken}`,
+          },
+        })
+        .then((response) => {
+          const newAccessToken = response.headers.authorization.substring(7);
+          const newRefreshToken = response.headers.refreshtoken.substring(7);
+          // 새로운 액세스 토큰과 리프레시 토큰을 로컬 스토리지에 업데이트합니다.
+          localStorage.setItem("accessToken", newAccessToken);
+          localStorage.setItem("refreshToken", newRefreshToken);
+          // 상태 업데이트
+          this.token.accessToken = response.headers.authorization.substring(7);
+          this.token.refreshToken = response.headers.refreshtoken.substring(7);
+          // 페이지내 데이터는 그대로 + 리로드
+          this.$router.go(this.$router.currentRoute);
+        })
+    } else {
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          const userName = response.data.loginId;
+          this.userName = userName;
+        })
+    }
+  },
+  setup() {
+    const token = useTokenStore();
+
+    return {
+      token,
+    };
   },
 };
 </script>
